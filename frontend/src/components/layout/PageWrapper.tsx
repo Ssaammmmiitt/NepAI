60bb7dfdbd526fb5d95f0bc2ca4ce7b251cc626d
Parth <pp03232422@student.ku.edu.np>
2026-06-13 13:12:17 +0545

    dirty

--- frontend/src/components/layout/PageWrapper.tsx
+++
@@ -1,7 +0,0 @@
-interface PageWrapperProps {
-  children: React.ReactNode;
-}
-
-export function PageWrapper({ children }: PageWrapperProps) {
-  return <main className="flex-1 p-4 md:p-8 overflow-y-auto max-h-[calc(100vh-4rem)] md:max-h-[calc(100vh-4rem)]">{children}</main>;
-}
